657e6874f83b77e335243ef80d95fcb4a81bdab1
Yuval Eliav <yuvaleliav1@gmail.com>
2021-12-03 11:34:21 +0200

    first project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg" />
+    </div>
+  );
+}
+
+export default App;
